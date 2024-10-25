6d1b27ef5e17e97d54d779ec980e0bf1d4d4f111
BSJ <127711612+Byeonseojin@users.noreply.github.com>
2024-10-25 11:43:44 +0900

    6장:행정경계지도와 지진분포 출력 +7장:웹스크래핑

--- 1018.R
+++ 1018.R
@@ -22,7 +22,6 @@ m = leaflet() %>%
   addMarkers(lng=127.1576468, lat = 37.5534841, label = "강동경희대병원", popup = "서울특별시 강동구 동남로 892") %>%
   addMarkers(lng=127.1574515, lat = 37.5567333, label = "온조대왕문화체육관", popup = "서울특별시 강동구 고덕로 285")
 m
-
 quakes
 
 # quakes 데이터셋에 있는 경도, 위도 값을 사용하여 지도타일을 출력
@@ -121,7 +120,7 @@ ggplot(data = df_map)+
 # 데이터셋은 엑셀 팡일로 읽어와서 사용
 install.packages("openxlsx")
 library(openxlsx)
-df = read.xlsx("D:/RScripts/map_data/국내지진목록.xlsx")
+df = read.xlsx("D:/RScripts/map_data/국내지진목록.xlsx", sheet=1, startRow = 4, colNames = FALSE)
 head(df)
 
 # x8열에서 북한으로 시작하는 데이터의 행번호 추출
@@ -129,7 +128,7 @@ idx = grep("^북한",df$X8)
 # 북한지역의 x8열의 데이터를 확인
 df[idx, 'X8']
 # X8열에 북한으로 시작하는 행삭제
-df = df[-idx]
+df = df[-idx,]
 
 # df에 있는 6열과 7열의 데이터 중 N과 E를 삭제하는 방법
 df[,6]=gsub("N", "",df[,6])
@@ -137,5 +136,22 @@ df[,7]=gsub("E", "",df[,7])
 # 6, 7열의 값을 숫자형으로 변환
 df[,6] = as.numeric(df[,6])
 df[,7] = as.numeric(df[,7])
+ 
+#df[,6]
 
-df[,6]
+# 행정경계지도와 지진분포 출력
+# shapefile 읽어오기
+map = st_read("D:/RScripts/map_data/Z_NGII_N3A_G0010000.shp")
+
+# WGS84 좌표계로 변환(지도출력)
+map = st_transform(map, crs=4326)
+
+# 포인터 데이터를 sf격체로 변환(포인트 출력)
+df_sf = df%>%st_as_sf(coords = c("X7", "X6"), crs=4326)
+
+# 행정경계지도 출력
+ggplot()+
+  geom_sf(data=map, fill="white", alpha=0.5, color="black")+
+  geom_sf(data=df_sf,aes(size=X3), shape=21, fill="red", alpha=0.3, color="black")+
+  theme(legend.position="none")+
+  labs(title="지진분포", x="경도",y="위도")
